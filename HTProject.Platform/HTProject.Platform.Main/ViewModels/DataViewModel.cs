40e0b91fa44dad2419406718f93785bc6f42e120
liuhongtuo <hongtuoliu@163.com>
2023-10-04 22:22:21 +0800

    搭建项目界面主体框架

---
+++ HTProject.Platform/HTProject.Platform.Main/ViewModels/DataViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTProject.Platform.Main.ViewModels
+{
+    class DataViewModel
+    {
+    }
+}
