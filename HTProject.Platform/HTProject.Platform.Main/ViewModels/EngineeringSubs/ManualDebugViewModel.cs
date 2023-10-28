9a861258094c8959a8588e187a9266de5d5d1dcb
liuhongtuo <hongtuoliu@163.com>
2023-10-28 22:17:53 +0800

    添加Logging相关Package及优化界面布局

--- HTProject.Platform/HTProject.Platform.Main/ViewModels/EngineeringSubs/ManualDebugViewModel.cs
+++ HTProject.Platform/HTProject.Platform.Main/ViewModels/EngineeringSubs/ManualDebugViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using HTProject.Platform.Main.ViewModels.EngineeringSubs.DebugSubs;
 using PropertyChanged;
 using System;
 using System.Collections.Generic;
@@ -14,5 +15,57 @@ namespace HTProject.Platform.Main.ViewModels.EngineeringSubs
     [ImplementPropertyChanged]
     public class ManualDebugViewModel : Screen
     {
+        public Screen LoadingStationView { get; set; }
+        private LoadingStationViewModel _loadingStationViewModel;
+
+        public Screen UnloadingStationView { get; set; }
+        private UnloadingStationViewModel _unloadingStationViewModel;
+
+        public Screen CollectingStationView { get; set; }
+        private CollectingStationViewModel _collectingStationViewModel;
+
+        public Screen DispensingStationView { get; set; }
+        private DispensingStationViewModel _dispensingStationViewModel;
+
+        public Screen AssemblyStationView { get; set; }
+        private AssemblyStationViewModel _asssemblyStationViewModel;
+
+        public Screen RecheckStationView { get; set; }
+        private RecheckStationViewModel _recheckStationViewModel;
+
+
+        public ManualDebugViewModel()
+        {
+            if (LoadingStationView == null)
+            {
+                _loadingStationViewModel = IoC.Get<LoadingStationViewModel>();
+                LoadingStationView = _loadingStationViewModel;
+            }
+            if (UnloadingStationView == null)
+            {
+                _unloadingStationViewModel = IoC.Get<UnloadingStationViewModel>();
+                UnloadingStationView = _unloadingStationViewModel;
+            }
+            if (CollectingStationView == null)
+            {
+                _collectingStationViewModel = IoC.Get<CollectingStationViewModel>();
+                CollectingStationView = _collectingStationViewModel;
+            }
+            if (DispensingStationView == null)
+            {
+                _dispensingStationViewModel = IoC.Get<DispensingStationViewModel>();
+                DispensingStationView = _dispensingStationViewModel;
+            }
+            if (AssemblyStationView == null)
+            {
+                _asssemblyStationViewModel = IoC.Get<AssemblyStationViewModel>();
+                AssemblyStationView = _asssemblyStationViewModel;
+            }
+            if (RecheckStationView == null)
+            {
+                _recheckStationViewModel = IoC.Get<RecheckStationViewModel>();
+                RecheckStationView = _recheckStationViewModel;
+            }
+        }
     }
 }
